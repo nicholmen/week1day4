62df373aca1ea83b2afba0e5a7c6e9b7cefcb9d1
Thomas Nicholson <nicholmen@gmail.com>
2017-09-08 00:54:45 +0000

    adding sorting exercises after completing name and age sorting

---
+++ advancedSorting.js
@@ -0,0 +1,63 @@
+//Write a sorting function (sometimes called a custom comparator), that sorts the above array first by the name ascending alphabetically,
+// and in cases where the names are equal sort by descending age.
+//given inputs to sort:
+
+var students = [
+    { id: 1, name: "bruce",    age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex",     age: 22 },
+    { id: 4, name: "alex",     age: 30 }
+  ];
+ 
+  // sort by value
+  students.sort(function (a, b) {
+    return a.value - b.value;
+  });
+  
+  // sort by name
+  students.sort(function(a, b) {
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    // names must be equal
+    return 0;
+  });
+
+console.log(students)
+  
+//   // related code from MDN
+//   var items = [
+//     { name: 'Edward', value: 21 },
+//     { name: 'Sharpe', value: 37 },
+//     { name: 'And', value: 45 },
+//     { name: 'The', value: -12 },
+//     { name: 'Magnetic', value: 13 },
+//     { name: 'Zeros', value: 37 }
+//   ];
+  
+//   // sort by value
+//   items.sort(function (a, b) {
+//     return a.value - b.value;
+//   });
+  
+//   // sort by name
+//   items.sort(function(a, b) {
+//     var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+//     var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+//     if (nameA < nameB) {
+//       return -1;
+//     }
+//     if (nameA > nameB) {
+//       return 1;
+//     }
+  
+//     // names must be equal
+//     return 0;
+//   });
+
+//   console.log(items)
---
+++ customSorting.js
@@ -0,0 +1,11 @@
+var array = [10, 2, 5, 1, 9];
+array.sort(function(a,b) {
+    return a - b
+});
+console.log(array)
+
+// //Sort numerically and ascending:
+// var myarray=[25, 8, 7, 41]
+// myarray.sort(function(a,b){ //Array now becomes [7, 8, 25, 41]
+//     return a - b
+// })
